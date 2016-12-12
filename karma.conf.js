97e3dbae053a8ad1ac4b88c3738dfcda6313b8b2
Patricia Garcia <pat@patriciagarcia.me>
2016-12-12 17:23:05 +0100

    chore: remove unused karma-chrome setup

--- karma.conf.js
+++ karma.conf.js
@@ -23,19 +23,5 @@ module.exports = function (config) {
     autoWatch: false,
 
     browsers: ['PhantomJS']
-
-    // This block is needed to execute Chrome on Travis
-    // If you ever plan to use Chrome and Travis, you can keep it
-    // If not, you can safely remove it
-    // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
-    // if (configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
-      // configuration.customLaunchers = {
-        // 'chrome-travis-ci': {
-          // base: 'Chrome',
-          // flags: ['--no-sandbox']
-        // }
-      // }
-      // configuration.browsers = ['chrome-travis-ci']
-    // }
   })
 }
